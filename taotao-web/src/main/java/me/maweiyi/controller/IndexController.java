3208391876536a30c1f81a9d132f6a8e97e9fe20
maweiyi <ma823285807@gmail.com>
2017-05-24 17:13:54 +0800

    commit

---
+++ taotao-web/src/main/java/me/maweiyi/controller/IndexController.java
@@ -0,0 +1,19 @@
+package me.maweiyi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by maweiyi on 5/21/17.
+ */
+@RequestMapping("index")
+@Controller
+public class IndexController {
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView toIndex() {
+        ModelAndView mv = new ModelAndView("index");
+        return mv;
+    }
+}
